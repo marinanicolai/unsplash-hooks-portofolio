b67c112ca437c1d0994a54fa14540f24863eb537
marina <nicolaimarina@gmail.com>
2022-05-02 09:40:18 -0400

    testing the PR

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Nav from "./components/Nav";
 import Search from "./components/Search";
 import About from "./components/About";
 import Item from "./components/Item";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Search />} />
-          <Route path="about" element={<About />} />
-          <Route path="item/:id" element={<Item />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component() {
+  state = { images: [] };
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Search onSearch={this.handleSubmit} />} />
+            <Route path="about" element={<About />} />
+            <Route path="item/:id" element={<Item />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,11 +1,38 @@
 import React from "react";
 
-function Search() {
-  return (
-    <div>
-      <h1>Search</h1>
-    </div>
-  );
+class Search extends React.Component {
+  state = {
+    images: [],
+  };
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="search"
+            required
+          />
+        </form>
+        {this.state.images.map((image) => (
+          <div>
+            <img
+              src={image.urls.regular}
+              key={image.id}
+              alt={image.description}
+            />
+            <div>{image.alt_description}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Search;
