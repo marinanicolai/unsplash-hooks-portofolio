e6d2b2d6b605705a14f18b603431c943facab769
marina <nicolaimarina@gmail.com>
2022-05-25 14:40:08 -0400

    image item changes

--- src/components/ImageItem/index.js
+++ src/components/ImageItem/index.js
@@ -5,11 +5,12 @@ const Item = ({ item, index, likes, setCurrentImageIndex }) => {
   return (
     <ImageContainer>
       <Image
-        src={item.urls["small"]}
+        src={item.urls.small}
         alt="sample"
         className="img-responsive w-100"
         onClick={() => setCurrentImageIndex(index)}
       />
+    </ImageContainer>
   );
 };
 export default Item;
