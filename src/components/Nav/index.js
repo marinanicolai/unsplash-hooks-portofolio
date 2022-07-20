403380f9b2ad060ed7079911558b0f23d626309e
marina <nicolaimarina@gmail.com>
2022-07-20 10:15:23 -0400

    updates

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import SearchBar from "../SearchBar";
 import { Link } from "react-router-dom";
+import { Navigation, CameraImage } from "./Nav.styles";
 
 const Nav = () => {
   return (
-    <nav>
-      <Link to="/">Home-logo</Link>
+    <Navigation>
+      <Link to="/">
+        <CameraImage>
+          <img
+            alt="camera-img"
+            className="camera-image"
+            src="https://a-thousand-words-ia23oi68u-redliz5808.vercel.app/static/media/AThousandWordsLogoOnly.30fa16bc.png"
+          />
+        </CameraImage>
+      </Link>
       <SearchBar />
       <Link to="/favorites">Saved</Link>
-    </nav>
+    </Navigation>
   );
 };
 
--- src/pages/Photo/index.js
+++ src/pages/Photo/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import {
+  ImageContainer,
+  Image,
+  AuthorName,
+} from "../../components/ImageItem/ImageItem.styles";
 
 const Photo = () => {
-  return <div>this is a photo page</div>;
+  let location = useLocation();
+
+  console.log(location.state);
+  console.log(location.state.user.first_name);
+  console.log(location.state.user.profile_image.medium);
+  console.log(location.state.urls.regular);
+  console.log("likes", location.state.likes);
+  console.log("views", location.state.views);
+  return (
+    <div>
+      <ImageContainer>
+        <Image
+          src={location.state.user.profile_image.medium}
+          alt="name of the author"
+        />
+        <AuthorName>{location.state.user.first_name}</AuthorName>
+      </ImageContainer>
+      <p>
+        if location state undefined => use useNavigate , navigate on front page{" "}
+      </p>
+    </div>
+  );
 };
 
 export default Photo;
