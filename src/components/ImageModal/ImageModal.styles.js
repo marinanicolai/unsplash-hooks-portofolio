b41eab59ae799ecff338635bd98552b15d7196e6
marina <nicolaimarina@gmail.com>
2022-12-17 10:13:38 -0500

    edit the image in the modal

---
+++ src/components/ImageModal/ImageModal.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+  justify-content: center;
+  padding: 15px;
+  max-height: 80%;
+  border-radius: 25px;
+`;
--- src/components/ImageModal/index.js
+++ src/components/ImageModal/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-
-
+import { Image } from "./ImageModal.styles";
 
 const ImageModal = ({ item, index, likes, setCurrentImageIndex }) => {
-    return (
-      <div className="col-lg-12 col-md-12 mb-4 mb-lg-0">
-        <img
-          key={item.id}
-          src={item.urls.small}
-          alt="sample"
-          className="img-responsive w-100"
-          onClick={() => setCurrentImageIndex(index)}
-        />
-      </div>
-    );
-  };
+  return (
+    <div className="col-lg-12 col-md-12 mb-4 mb-lg-0">
+      <Image
+        key={item.id}
+        src={item.urls.small}
+        alt="sample"
+        className="img-responsive w-100"
+        onClick={() => setCurrentImageIndex(index)}
+      />
+    </div>
+  );
+};
 
 export default ImageModal;
