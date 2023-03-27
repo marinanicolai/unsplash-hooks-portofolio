d7437ed6934a3701fdd712794b7049c3a4e9c616
marina <nicolaimarina@gmail.com>
2023-03-27 10:42:08 -0400

    edit the numbers of images displayed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ import { useFetch } from "../../hooks/useFetch";
 const Home = () => {
   const [query] = useState(null);
   const url = query ? `/search/photos` : "/photos/random";
-  const { data, isLoading, isError } = useFetch(`${url}/?count=10`, query);
+  const { data, isLoading, isError } = useFetch(`${url}/?count=12`, query);
   return (
     <div>
       {isLoading && <Loading />}
